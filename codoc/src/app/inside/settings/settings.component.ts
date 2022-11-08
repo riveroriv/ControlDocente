90f233ee28e49b5c26238b0c84ee3efcbae0eb8f
riveror <richardrivero@upb.edu>
2022-11-08 15:41:19 -0400

    ciudades

--- codoc/src/app/inside/settings/settings.component.ts
+++ codoc/src/app/inside/settings/settings.component.ts
@@ -5,12 +5,19 @@ import { Router } from '@angular/router';
 import { AuthService } from 'src/app/servicios/auth.service';
 import { CiudadService } from 'src/app/servicios/ciudad.service';
 
+export interface Ciudad {
+  id: number,
+  nombre: string
+}
+
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+
+  ciudades: Ciudad[] = Array();
 
   constructor(
     public authService: AuthService,
@@ -31,9 +38,28 @@ export class SettingsComponent implements OnInit {
   cargarCiudades(){
     this.ciudadService.getCiudades().subscribe({
       next: (data) => {
-        console.log(data);
+        let ciudades: any = data;
+        this.ciudades = ciudades;
       }
     });
+  }
+  actualizar(id: number, nuevoNombre: string){
+    this.ciudadService.actualizarCiudad(id, nuevoNombre).subscribe({
+      next: (r) => {
+        this.snackBar('Ciudad actualizada');
+        this.cargarCiudades();
+      },
+      error: (r) => this.snackBar('No se pudo guardar cambio')
+    });    
+  }
+  nuevaCiudad(nuevoNombre: string){
+    this.ciudadService.crearCiudad(nuevoNombre).subscribe({
+      next: (r) => {
+        this.snackBar('Ciudad creada');
+        this.cargarCiudades();
+      },
+      error: (r) => this.snackBar('No se pudo crear')
+    });    
   }
   snackBar(message: string){
     this._snackBar.open(message, 'Cerrar', {
